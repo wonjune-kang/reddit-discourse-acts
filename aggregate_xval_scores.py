1107da363dfe38a231e8286cbeb7cde5fdcc33f9
Wonjune Kang <wjkang@media.mit.edu>
2021-02-12 21:15:13 +0000

    cleanup

--- aggregate_xval_scores.py
+++ aggregate_xval_scores.py
@@ -2,7 +2,8 @@ import sys
 import pandas as pd
 
 
-score_file = sys.argv[1]
-scores_df = pd.read_csv(score_file, delimiter="\t").drop('Fold', axis=1)
-xval_avg_scores = scores_df.mean()
-print(xval_avg_scores)
+if __name__ == '__main__':
+    score_file = sys.argv[1]
+    scores_df = pd.read_csv(score_file, delimiter="\t").drop('Fold', axis=1)
+    xval_avg_scores = scores_df.mean()
+    print(xval_avg_scores)
