662b8759da31467b4273f75eeb5f90f0d52dbc95
Wonjune Kang <wjkang@media.mit.edu>
2021-02-12 20:32:28 +0000

    average scores from cross validation folds

---
+++ aggregate_xval_scores.py
@@ -0,0 +1,8 @@
+import sys
+import pandas as pd
+
+
+score_file = sys.argv[1]
+scores_df = pd.read_csv(score_file, delimiter="\t").drop('Fold', axis=1)
+xval_avg_scores = scores_df.mean()
+print(xval_avg_scores)
